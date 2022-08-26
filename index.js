a1deac7a7c16cea338e01fbfed2fac546a79b0db
Nicolas DiMarco <kikecarc17@gmail.com>
2022-08-25 23:52:54 -0300

    ,.,.,,

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require("express");
-const routerProducts = require("./products.router");
 const routerCarrito = require("./carrito.router");
 const routerAdmin = require("./admin.router");
 
-const fs = require("fs");
-
 const PORT = 8080;
-
-const productos = require("./products/products.json");
-const carrito = require("./carrito/carrito.json");
 
 const app = express();
 
@@ -19,7 +13,6 @@ app.use(express.urlencoded({ extended: "true" }));
 
 //routes //
 
-app.use("/api/products", routerProducts);
 app.use("/api/carrito", routerCarrito);
 app.use("/api/admin", routerAdmin);
 
