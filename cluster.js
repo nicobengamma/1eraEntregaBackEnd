a1092b7955f028c52ba48ccb50997a463f7b76e6
nicobengamma <kikecarc17@gmail.com>
2022-10-03 13:49:38 -0300

    .,.,

---
+++ cluster.js
@@ -0,0 +1,23 @@
+const numCpus = require("os").cpus().length;
+const cluster = require("cluster");
+const http = require("http");
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died :()`);
+  });
+} else {
+  http
+    .createServer((req, res) => {
+      console.log(`Entro en el proceso ${process.pid}`);
+      res.writeHead(200);
+      res.end("hello ");
+    })
+    .listen(8080);
+  console.log(`worker ${process.pid} started`);
+}
